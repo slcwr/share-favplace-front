e37754f1b349415e0098633b5545d53e6f14b036
a.y <g.slackware@gmail.com>
2024-05-17 07:13:03 +0900

    AxiosでAPIリクエスト

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useState } from 'react';
+import axios from './configs/axios';
+
 export default function App() {
+  const [text, setText] = useState('');
+
+  const handleClick = () => {
+    axios.get('/api/v1/hello')
+      .then((res) => {
+        setText(res.data)
+      })
+  };
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <div className='flex justify-center mt-10'>
+        <button type="button" className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" onClick={() => handleClick()}>Click!</button>
+      </div>
+      <div className="flex justify-center mt-5">
+        <h1 className="text-3xl font-bold underline">
+          { text }
+        </h1>
+      </div>
+    </div>
   )
 }
