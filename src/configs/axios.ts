97af9cbe24cb9d190cf55279d8860523c923a206
a.y <g.slackware@gmail.com>
2024-05-18 06:38:45 +0900

    nodemodule追加

--- src/configs/axios.ts
+++ src/configs/axios.ts
@@ -1,7 +1,6 @@
 import { editableInputTypes } from "@testing-library/user-event/dist/utils";
 import axios from "axios";
 
-
 let instance = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
 });
@@ -13,7 +12,7 @@ if (process.env.REACT_APP_ENV !== 'production') {
       console.log(config);
       return config;
     },
-    (error: string) => {
+    (error) => {
       console.log(error);
       return Promise.reject(error);
     }
