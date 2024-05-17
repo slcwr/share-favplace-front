0e97107856895c3d6fcd3a0f73e2e7059381f703
a.y <g.slackware@gmail.com>
2024-05-17 14:10:26 +0900

    Dockerfile変更

---
+++ src/configs/axios.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+let instance = axios.create({
+  baseURL: process.env.REACT_APP_API_URL,
+});
+
+// 本番環境以外はリクエストとレスポンス時にデバッグ用ログを出力する
+if (process.env.REACT_APP_ENV !== 'production') {
+  instance.interceptors.request.use(
+    (config) => {
+      console.log(config);
+      return config;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
+
+  instance.interceptors.response.use(
+    (response) => {
+      console.log(response);
+      return response;
+    },
+    (error) => {
+      console.log(error);
+      return Promise.reject(error);
+    }
+  );
+}
+
+export default instance;
