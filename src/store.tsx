e7278f6df6284d666673932dcecc44c1ffad0724
a.y <g.slackware@gmail.com>
2024-06-12 22:09:29 +0900

    ローカルストレージ

--- src/store.tsx
+++ src/store.tsx
@@ -3,12 +3,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import modalReducer from './features/modal/modalSlice';
 import { save, load } from 'redux-localstorage-simple';
+import rootReducer from './rootreducer';
+
 
 export const store = configureStore({
-  reducer: {
-    cart: cartReducer,
-    modal: modalReducer,
-  },
+  reducer: rootReducer,
   preloadedState: load(),
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(save()),
 });
