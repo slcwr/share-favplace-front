126cee807574f4dd69d3ef65bc8a5cd87605b8d6
a.y <g.slackware@gmail.com>
2024-06-24 21:22:44 +0900

    注文もーだる

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -24,6 +24,24 @@ const CartModal = () => {
     <aside className="modal-container">
         <div className="modal add">
             <h4>買い物かご</h4>
+            <div>
+      {cartItems.map((item) => {
+          // idがundefinedの場合の対処
+          if (item.id === undefined) {
+            return null;
+          }
+          // idがある場合に限り、CartItemをレンダリング
+          const cartItemProps: CartItemType = {
+            id: item.id,
+            img: item.img,
+            title: item.title,
+            price: item.price,
+            amount: item.amount || 0, // amountがundefinedの場合に0を代入
+          };
+          return <CartItem key={item.id} {...cartItemProps} />;
+        })}
+
+      </div>
             <div className="btn-container">
                 <button className="btn clear-btn" onClick={() => dispatch(closeModal())}>戻る</button>
             </div>
